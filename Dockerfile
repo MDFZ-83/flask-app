c18d723e925e01490378e4f7eff1b058eb0e0c9b
mohammadfz <feizimohammad83@gmail.com>
2025-10-09 10:06:17 +0330

    Add initial Flask app with Docker and Terraform setup
    - Created CI/CD pipeline in docker.yml
    - Added Dockerfile for Flask application
    - Implemented basic Flask app in app.py
    - Configured AWS provider and resources in main.tf
    - Defined VPC CIDR block variable in variable.tf
    - Updated .gitignore to exclude Terraform files
    - Specified Flask dependency in requirements.txt

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# Use official Python image
+FROM python:3.10-slim
+
+WORKDIR /app
+COPY requirements.txt requirements.txt
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "app.py"]
---
+++ iteraform/main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "mfz"
+  secret_key                  = "mfz"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    apigatewayv2   = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    ec2            = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    elasticache    = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+
+  }
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_instance" "mfz-instance" {
+  ami = "ami-03025bb25a1de0fc2"
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.devops-mfz-sc.name]
+
+}
+
+resource "aws_security_group" "devops-mfz-sc" {
+  name = "mfz-sc"
+  description = "Allow SSH and HTTP"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
